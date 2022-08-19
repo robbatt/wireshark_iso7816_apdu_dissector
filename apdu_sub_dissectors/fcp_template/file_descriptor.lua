ce7f893f6422a9f6cfab9881cc1d3126e135c046
Robert Brendler <robert.brendler@gmail.com>
2022-08-19 22:53:39 +0200

    moved local constants to file_descriptor dissector

--- apdu_sub_dissectors/fcp_template/file_descriptor.lua
+++ apdu_sub_dissectors/fcp_template/file_descriptor.lua
@@ -1,3 +1,25 @@
+local FILE_TYPE = {
+    [0] = "Working EF",
+    [1] = "Internal EF",
+    [2] = RFU,
+    [3] = RFU,
+    [4] = RFU,
+    [5] = RFU,
+    [6] = RFU,
+    [7] = "DF or ADF",
+}
+local EF_STRUCTURE = {
+    [0] = "No information given",
+    [1] = "Transparent structure",
+    [2] = "Linear fixed structure",
+    [3] = RFU,
+    [4] = RFU,
+    [5] = RFU,
+    [6] = "Cyclic structure",
+    [7] = RFU,
+    [9] = "BER-TLV structure" -- TODO this should actually be 57 0-111001
+}
+
 local p = Proto.new("iso7816.apdu.file_descriptor", "File Descriptor")
 local pf = {
     section = ProtoField.string(p.name .. ".section", "Section"),
--- constants.lua
+++ constants.lua
@@ -3,27 +3,6 @@ YES_NO = {
     [1] = "Yes"
 }
 
-FILE_TYPE = {
-    [0] = "Working EF",
-    [1] = "Internal EF",
-    [2] = RFU,
-    [3] = RFU,
-    [4] = RFU,
-    [5] = RFU,
-    [6] = RFU,
-    [7] = "DF or ADF",
-}
-EF_STRUCTURE = {
-    [0] = "No information given",
-    [1] = "Transparent structure",
-    [2] = "Linear fixed structure",
-    [3] = RFU,
-    [4] = RFU,
-    [5] = RFU,
-    [6] = "Cyclic structure",
-    [7] = RFU,
-    [9] = "BER-TLV structure" -- TODO this should actually be 57 0-111001
-}
 FILE_IDENTIFIERS = {
     [0x3F00] = 'MF',
     [0x2F00] = 'EF.DIR',
