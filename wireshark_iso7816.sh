66238f22410493375493d1d8a478d9ee87ce904d
Robert Brendler <robert.brendler@gmail.com>
2022-10-04 19:02:49 +0200

    fix run script after refactoring

--- wireshark_iso7816.sh
+++ wireshark_iso7816.sh
@@ -1,3 +1,3 @@
 #! /bin/bash
 
-wireshark -X lua_script:iso7816_adpu.lua -r simtrace2_bg95_boot_roam_profile.pcapng
+wireshark -X lua_script:iso7816_apdu.lua -r reference_traces/simtrace2_bg95_boot_roam_profile.pcapng
