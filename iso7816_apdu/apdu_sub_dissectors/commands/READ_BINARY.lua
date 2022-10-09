8bcc2f2c6797f02b2baef55cc12fe6cff3b82c71
Robert Brendler <robert.brendler@gmail.com>
2022-10-09 23:37:51 +0200

    added PSLOCI file parser

--- iso7816_apdu/apdu_sub_dissectors/commands/READ_BINARY.lua
+++ iso7816_apdu/apdu_sub_dissectors/commands/READ_BINARY.lua
@@ -6,6 +6,7 @@ local dt_parsers = DissectorTable.new('iso7816.apdu.file_parsers', 'ISO7816-APDU
 dt_parsers:add(0x2fe2, require('apdu_sub_dissectors/file_parsers/ICCID'))
 dt_parsers:add(0x6f07, require('apdu_sub_dissectors/file_parsers/IMSI'))
 dt_parsers:add(0x6f7e, require('apdu_sub_dissectors/file_parsers/LOCI'))
+dt_parsers:add(0x6f73, require('apdu_sub_dissectors/file_parsers/PSLOCI'))
 
 local p = Proto.new("iso7816.apdu.instructions.READ_BINARY", "READ_BINARY")
 local pf = {
---
+++ iso7816_apdu/apdu_sub_dissectors/file_parsers/PSLOCI.lua
@@ -0,0 +1,47 @@
+-- prevent wireshark loading this file as a plugin
+if not _G['iso7816_apdu'] then
+    return
+end
+
+local ROUTING_AREA_UPDATE_STATUS = {
+    [0x02] = 'PLMN not allowed'
+}
+
+local p = Proto.new("iso7816.apdu.file_parsers.psloci", "PSLOCI (Packet Switched Location Information):")
+local pf = {
+    p_tmsi = ProtoField.uint32(p.name .. ".p_tmsi", "P-TMSI", base.HEX),
+    p_tmsi_sig = ProtoField.uint24(p.name .. ".p_tmsi_signature", "P-TMSI signature value", base.HEX),
+    --LAI (Location Area Information)
+    mcc_mnc = ProtoField.string(p.name .. ".mcc_mnc", "MCC & MNC"),
+    lac = ProtoField.uint16(p.name .. ".lac", "LAC", base.HEX),
+
+    rac = ProtoField.uint8(p.name .. ".rac", "RAC", base.HEX),
+    routing_area_update_status = ProtoField.uint8(p.name .. ".routing_area_update_status", "Routing area update status", base.HEX, ROUTING_AREA_UPDATE_STATUS),
+}
+p.fields = pf
+
+
+function p.dissector(buffer, pinfo, tree)
+    local p_tmsi_f = buffer:range(0, 4)
+    local p_tmsi_sig_f = buffer:range(4, 3)
+    local mcc_mnc_f = buffer:range(7, 3)
+    local lac_f = buffer:range(10, 2)
+    local rac_f = buffer:range(12, 1)
+    local status_f = buffer:range(13, 1)
+
+    local mcc = mcc(mcc_mnc_f)
+    local mnc = mnc(mcc_mnc_f)
+
+    -- optional, add a new level (dropdown) for this section
+    local subtree = tree:add(p, buffer, string.format('MCC/MNC: %s/%s', mcc, mnc))
+    subtree:add(pf.p_tmsi, p_tmsi_f)
+    subtree:add(pf.p_tmsi_sig, p_tmsi_sig_f)
+    subtree:add(pf.mcc_mnc, mcc_mnc_f, string.format('%s/%s (0x%06x)',mcc, mnc, mcc_mnc_f:uint()))
+    subtree:add(pf.lac, lac_f)
+    subtree:add(pf.rac, rac_f)
+    subtree:add(pf.routing_area_update_status, status_f)
+
+    return buffer:len() -- processed bytes
+end
+
+return p -- returning protocol to add it into dissector table with require
